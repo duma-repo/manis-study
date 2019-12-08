92b926caae8450059f41bbe7e8839a6501abcf16
duma-repo <zhuimengstudio@163.com>
2019-12-08 16:24:09 +0800

    update section-14

--- src/main/java/com/cnblogs/duma/ipc/Server.java
+++ src/main/java/com/cnblogs/duma/ipc/Server.java
@@ -1,13 +1,17 @@
 package com.cnblogs.duma.ipc;
 
+import com.cnblogs.duma.conf.CommonConfigurationKeysPublic;
 import com.cnblogs.duma.conf.Configuration;
 import com.cnblogs.duma.io.Writable;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * @author duma
@@ -19,7 +23,24 @@ public abstract class Server {
     private int handlerCount;
     private int readThreads;
 
+    /** callQueue 队列的大小 */
+    private int maxQueueSize;
+    /** 响应 buffer 的上限 */
+    private int maxRespSize;
+    /** 客户端信息长度的上限 */
+    private int maxDataLength;
+    private boolean tcpNoDelay;
+    /** pendingConnections 队列的大小 */
+    private int readerPendingConnectionQueue;
+
+    private BlockingQueue<Call> callQueue;
+
     volatile private boolean running = true;
+
+    private ConnectionManager connectionManager;
+    private Listener listener;
+    private Responder responder;
+    private Handler[] handlers = null;
 
     private Configuration conf;
 
@@ -69,13 +90,60 @@ public abstract class Server {
     protected Server(String bindAddress, int port,
                      int numHandlers, int numReaders, int queueSizePerHandler,
                      Configuration conf) throws IOException {
+        this.conf = conf;
+        this.bindAddress = bindAddress;
+        this.port = port;
+        this.handlerCount = numHandlers;
+
+        if (numReaders != -1) {
+            this.readThreads = numReaders;
+        } else {
+            this.readThreads = conf.getInt(
+                    CommonConfigurationKeysPublic.IPC_SERVER_RPC_READ_THREADS_KEY,
+                    CommonConfigurationKeysPublic.IPC_SERVER_RPC_READ_THREADS_DEFAULT);
+        }
+        if (queueSizePerHandler != -1) {
+            this.maxQueueSize = numHandlers * queueSizePerHandler;
+        } else {
+            this.maxQueueSize = numHandlers * conf.getInt(
+                    CommonConfigurationKeysPublic.IPC_SERVER_HANDLER_QUEUE_SIZE_KEY,
+                    CommonConfigurationKeysPublic.IPC_SERVER_HANDLER_QUEUE_SIZE_DEFAULT);
+        }
+        this.maxDataLength = conf.getInt(
+                CommonConfigurationKeysPublic.IPC_MAXIMUM_DATA_LENGTH,
+                CommonConfigurationKeysPublic.IPC_MAXIMUM_DATA_LENGTH_DEFAULT);
+        this.maxRespSize = conf.getInt(
+                CommonConfigurationKeysPublic.IPC_SERVER_RPC_MAX_RESPONSE_SIZE_KEY,
+                CommonConfigurationKeysPublic.IPC_SERVER_RPC_MAX_RESPONSE_SIZE_DEFAULT);
+        this.readerPendingConnectionQueue = conf.getInt(
+                CommonConfigurationKeysPublic.IPC_SERVER_RPC_READ_CONNECTION_QUEUE_SIZE_KEY,
+                CommonConfigurationKeysPublic.IPC_SERVER_RPC_READ_CONNECTION_QUEUE_SIZE_DEFAULT);
+        this.callQueue = new LinkedBlockingDeque<>(maxQueueSize);
+
+        // 创建 listener
+        this.listener = new Listener();
+        this.connectionManager = new ConnectionManager();
+
+        this.tcpNoDelay = conf.getBoolean(
+                CommonConfigurationKeysPublic.IPC_CLIENT_TCPNODELAY_KEY,
+                CommonConfigurationKeysPublic.IPC_CLIENT_TCPNODELAY_DEFAULT);
+
+        // 创建 responder
+        this.responder = new Responder();
     }
 
     /**
      * 启动服务
      */
     public synchronized void start() {
+        listener.start();
+        responder.start();
+        handlers = new Handler[handlerCount];
 
+        for (int i = 0; i < handlerCount; i++) {
+            handlers[i] = new Handler(i);
+            handlers[i].start();
+        }
     }
 
     /**
@@ -86,5 +154,78 @@ public abstract class Server {
         while (running) {
             wait();
         }
+    }
+
+    public static class Call {
+        private final int callId;
+        private final int retryCount;
+        /** 客户端发来的序列化的 RPC 请求 */
+        private final Writable rpcRequest;
+        private final Connection connection;
+        private long timestamp;
+
+        /** 本次调用的响应信息 */
+        private ByteBuffer response;
+        private final RPC.RpcKind rpcKind;
+        private final byte[] clientId;
+
+        public Call(int id, int retryCount, Writable rpcRequest, Connection connection) {
+            this(id, retryCount, rpcRequest, connection,
+                    RPC.RpcKind.RPC_BUILTIN, RpcConstants.DUMMY_CLIENT_ID);
+        }
+
+        public Call(int id, int retryCount, Writable rpcRequest,
+                    Connection connection, RPC.RpcKind rpcKind, byte[] clientId) {
+            this.callId = id;
+            this.retryCount = retryCount;
+            this.rpcRequest = rpcRequest;
+            this.connection = connection;
+            this.timestamp = System.currentTimeMillis();
+            this.response = null;
+            this.rpcKind = rpcKind;
+            this.clientId = clientId;
+        }
+
+        public void setResponse(ByteBuffer response) {
+            this.response = response;
+        }
+
+        @Override
+        public String toString() {
+            return rpcRequest + " from " + connection + " Call#" + callId +
+                    " Retry#" + retryCount;
+        }
+    }
+
+    /**
+     * socket 监听线程
+     */
+    private class Listener extends Thread {
+
+    }
+
+    /**
+     * 处理队列中的调用请求
+     */
+    private class Handler extends Thread {
+        Handler(int instanceNumber) {
+            this.setDaemon(true);
+            this.setName("IPC Server handler " + instanceNumber + " on " + port);
+        }
+    }
+
+    /**
+     * 向客户端发送响应结果
+     */
+    private class Responder extends Thread {
+
+    }
+
+    private class Connection {
+
+    }
+
+    private class ConnectionManager {
+
     }
 }
